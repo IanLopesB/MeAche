7343e1501fb1cd61b6bac8e1e1c4f4379022a82d
wpaq <wallysonpabloo@gmail.com>
2018-09-05 17:43:07 -0300

    att

--- wpaq/admin/_js/checkbox.js
+++ wpaq/admin/_js/checkbox.js
@@ -1,50 +1,57 @@
       function check_tb1() {
-        var ch = document.getElementById("checkb1");
-        if (ch.checked == true) {
-          document.getElementById("tb1").style.textDecoration = "line-through";
-          document.getElementById("tb1").style.color = "grey";
+        var checkBox = document.getElementById("checkb1");
+        var id = document.getElementById("tb1");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
+          id.style.display = "none";
         }
         else {
-          document.getElementById("tb1").style.textDecoration = "none";
-          document.getElementById("tb1").style.color = "black";
+          id.style.textDecoration = "none";
+          id.style.color = "black";
         }
       }
 
       function check_tb2() {
-        var ch = document.getElementById("checkb2");
-        if (ch.checked == true) {
-          document.getElementById("tb2").style.textDecoration = "line-through";
-          document.getElementById("tb2").style.color = "grey";
+        var checkBox = document.getElementById("checkb2");
+        var id = document.getElementById("tb2");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
         }
         else {
-          document.getElementById("tb2").style.textDecoration = "none";
-          document.getElementById("tb2").style.color = "black";
-        }
+          id.style.textDecoration = "none";
+          id.style.color = "black";
 
+        }
       }
 
       function check_tb3() {
-        var ch = document.getElementById("checkb3");
-        if (ch.checked == true) {
-          document.getElementById("tb3").style.textDecoration = "line-through";
-          document.getElementById("tb3").style.color = "grey";
+        var checkBox = document.getElementById("checkb3");
+        var id = document.getElementById("tb3");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
         }
         else {
-          document.getElementById("tb3").style.textDecoration = "none";
-          document.getElementById("tb3").style.color = "black";
+          id.style.textDecoration = "none";
+          id.style.color = "black";
         }
-
       }
 
       function check_tb4() {
-        var ch = document.getElementById("checkb4");
-        if (ch.checked == true) {
-          document.getElementById("tb4").style.textDecoration = "line-through";
-          document.getElementById("tb4").style.color = "grey";
+        var checkBox = document.getElementById("checkb4");
+        var id = document.getElementById("tb4");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
         }
         else {
-          document.getElementById("tb4").style.textDecoration = "none";
-          document.getElementById("tb4").style.color = "black";
+          id.style.textDecoration = "none";
+          id.style.color = "black";
         }
-
       }
