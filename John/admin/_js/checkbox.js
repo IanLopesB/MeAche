eb931795e1dad4b854efe8b38cfd71596449286c
Johnny <Johnny.tec13@gmail.com>
2018-09-06 14:46:06 -0300

    Upando a primeira vez

---
+++ John/admin/_js/checkbox.js
@@ -0,0 +1,57 @@
+      function check_tb1() {
+        var checkBox = document.getElementById("checkb1");
+        var id = document.getElementById("tb1");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
+          id.style.display = "none";
+        }
+        else {
+          id.style.textDecoration = "none";
+          id.style.color = "black";
+        }
+      }
+
+      function check_tb2() {
+        var checkBox = document.getElementById("checkb2");
+        var id = document.getElementById("tb2");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
+        }
+        else {
+          id.style.textDecoration = "none";
+          id.style.color = "black";
+
+        }
+      }
+
+      function check_tb3() {
+        var checkBox = document.getElementById("checkb3");
+        var id = document.getElementById("tb3");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
+        }
+        else {
+          id.style.textDecoration = "none";
+          id.style.color = "black";
+        }
+      }
+
+      function check_tb4() {
+        var checkBox = document.getElementById("checkb4");
+        var id = document.getElementById("tb4");
+
+        if (checkBox.checked == true) {
+          id.style.textDecoration = "line-through";
+          id.style.color = "grey";
+        }
+        else {
+          id.style.textDecoration = "none";
+          id.style.color = "black";
+        }
+      }
