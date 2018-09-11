1cb2f7827ae01924f79cdae68f8947e650e949d9
Johnny <Johnny.tec13@gmail.com>
2018-09-11 17:53:52 -0300

    Upando

---
+++ wpaq/admin/_js/connection.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.4.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8fBbF_xn3q33YdNWY2oDCQ3D9QpNFPO4",
+    authDomain: "me-ache-6c62e.firebaseapp.com",
+    databaseURL: "https://me-ache-6c62e.firebaseio.com",
+    projectId: "me-ache-6c62e",
+    storageBucket: "me-ache-6c62e.appspot.com",
+    messagingSenderId: "164197814660"
+  };
+  firebase.initializeApp(config);
+</script>
