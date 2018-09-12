aa921172eb245354bbb817fbe2d86d28b48f8b1f
wpaq <wallysonpabloo@gmail.com>
2018-09-12 17:51:24 -0300

    att 1.4.3

--- wpaq/admin/_js/connection.js
+++ wpaq/admin/_js/connection.js
@@ -1,10 +1,14 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA8fBbF_xn3q33YdNWY2oDCQ3D9QpNFPO4",
-    authDomain: "me-ache-6c62e.firebaseapp.com",
-    databaseURL: "https://me-ache-6c62e.firebaseio.com",
-    projectId: "me-ache-6c62e",
-    storageBucket: "me-ache-6c62e.appspot.com",
-    messagingSenderId: "164197814660"
-  };
-  firebase.initializeApp(config);
+  (function() { 
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA8fBbF_xn3q33YdNWY2oDCQ3D9QpNFPO4",
+      authDomain: "me-ache-6c62e.firebaseapp.com",
+      databaseURL: "https://me-ache-6c62e.firebaseio.com",
+      projectId: "me-ache-6c62e",
+      storageBucket: "me-ache-6c62e.appspot.com",
+      messagingSenderId: "164197814660"
+    };
+    firebase.initializeApp(config);
+
+  }());
