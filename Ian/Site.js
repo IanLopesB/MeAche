3e052781d1072773abd2387125a127f8a3626e66
IanLopesB <ianlopes246@gmail.com>
2018-09-12 17:04:16 -0300

    att

--- Ian/Site.js
+++ Ian/Site.js
@@ -74,3 +74,12 @@ var x = document.getElementById("prato1");
        }
    
    }
+
+   function abrirMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
