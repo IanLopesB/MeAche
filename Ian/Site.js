e966b61f2cb18172b4d080f4e5f7b6bbbf34667f
IanLopesB <ianlopes246@gmail.com>
2018-09-12 17:54:46 -0300

    att

--- Ian/Site.js
+++ Ian/Site.js
@@ -83,3 +83,21 @@ var x = document.getElementById("prato1");
         x.className = "topnav";
     }
 }
+
+
+
+
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace("", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += "";
+}
+document.getElementById("Cat1").click();
