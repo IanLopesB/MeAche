13a2229d7a99f13d786970dfd6b7af9270d37fdf
Johnny <Johnny.tec13@gmail.com>
2018-09-24 16:06:00 -0300

    Upando

--- wpaq/MeAche/_js/checkbox.js
+++ wpaq/MeAche/_js/checkbox.js
@@ -2,7 +2,7 @@ function check_tb(value){
   var inputs = document.getElementById("checkb" + value);
   var tr = document.getElementById("tr"+ value);
 
-    if (inputs.checked == true) {        
+    if (inputs.checked == true) {
       tr.style.textDecoration = "line-through";
       tr.style.color = "red";
       }
