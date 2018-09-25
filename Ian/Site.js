c7b7416fc63fb8829b2ec421574df56dfa493d5e
Thalita <thalitasilva877@gmail.com>
2018-09-25 14:35:51 -0300

    jjjj

--- Ian/Site.js
+++ Ian/Site.js
@@ -93,9 +93,9 @@ function openMenu(evt, menuName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+     tablinks[i].className = tablinks[i].className.replace(" w3-black", "");
   }
   document.getElementById(menuName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-red";
+  evt.currentTarget.firstElementChild.className += " w3-black";
 }
 document.getElementById("Cat1").click();
