57356c660dd6af676add6195c232caa7d1106ef3
wpaq <wallysonpabloo@gmail.com>
2018-09-26 17:01:00 -0300

    att 2.0.4

--- wpaq/MeAche/_js/createTable.js
+++ wpaq/MeAche/_js/createTable.js
@@ -4,27 +4,29 @@ firebase.database().ref('pedidos').on('value', function (snapshot) {
 
   var tr = document.createElement('tr');
   var th = document.createElement('th');
-  var input = document.createElement('input');
-  var td = document.createElement('td');
-  var td1 = document.createElement('td');
+  var input_checkbox = document.createElement('input');
+  var td_pedidos = document.createElement('td');
+  var td_num_pedidos = document.createElement('td');
   var p = document.createElement('p');
-  var br = document.createElement('br');
-  var text = document.createTextNode("Asinha de Frango: " + item.val().Asinha_de_frango + "x");
-  var text1 = document.createTextNode("Camarão Alho e Óleo:  " + item.val().Camarao_Alho_Oleo + "x");
 
+  var linebreak = document.createElement('br');
+  var space = document.createTextNode('');
+  
+  var petiscos_1 = document.createTextNode(item.val().Asinha_de_frango + "x" + " Asinha de Frango" ); 
+  var petiscos_2 = document.createTextNode("Camarão Alho e Óleo:  " + item.val().Camarao_Alho_Oleo + "x");
 
   var trbody = document.getElementById('tableAdm');
 
   trbody.appendChild(tr);
   tr.appendChild(th);
-  tr.appendChild(td);
-  tr.appendChild(td1);
-  th.appendChild(input);
-  td.appendChild(p);
+  tr.appendChild(td_pedidos);
+  tr.appendChild(td_num_pedidos);
+  th.appendChild(input_checkbox);
+  td_pedidos.appendChild(p);
   th.setAttribute("scope", "row");
 
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("onclick", "check_tb(this.value)");
+  input_checkbox.setAttribute("type", "checkbox");
+  input_checkbox.setAttribute("onclick", "check_tb(this.value)");
 
   tr.setAttribute("class", "trClass");
 
@@ -34,17 +36,18 @@ firebase.database().ref('pedidos').on('value', function (snapshot) {
       trId[i].id = "tr" + (i + 1);
   }
 
-  input.setAttribute("class", "inputClass");
+  input_checkbox.setAttribute("class", "inputClass");
   var inputId = document.getElementsByClassName("inputClass");
     for (var i = 0; i < trId.length; i++) {
       inputId[i].id = "checkb" + (i + 1);
   }
 
-  td.setAttribute("colspan", "2");
-  p.appendChild(text).innerHTML = "<br>";
-  p.appendChild(text1);
+  td_pedidos.setAttribute("colspan", "2");
+  p.appendChild(petiscos_1);
+  p.appendChild(linebreak);
+  p.appendChild(petiscos_2);
 
-  input.setAttribute("name", "number");
+  input_checkbox.setAttribute("name", "number");
 
   $("input[name='number']").each(function(ind) {
    $(this).val(ind + 1);
