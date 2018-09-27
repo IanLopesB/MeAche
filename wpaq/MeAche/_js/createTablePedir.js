51cd2198f5918a360c987ba59266ca3c75dd5e2c
Johnny <Johnny.tec13@gmail.com>
2018-09-27 15:37:04 -0300

    Upando

--- wpaq/MeAche/_js/createTablePedir.js
+++ wpaq/MeAche/_js/createTablePedir.js
@@ -1,14 +1,3 @@
-/*function mandardados(){
-var text = document.getElementById('quantPet2').value;
-var qtd = document.getElementById('qtd2');
-qtd.innerText = text;
-
-var camarao = document.getElementById('camarao').firstChild.innerHTML;
-document.getElementById('Pedido2').innerHTML = camarao;
-
-}
-*/
-
 function mandardados1(){
 
 var value1 = document.getElementById('quantPet1').value;
