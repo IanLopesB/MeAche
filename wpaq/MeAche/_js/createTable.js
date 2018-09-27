2bfc7a4e2fcebcc151fe7157a30fff3b2b42c544
wpaq <wallysonpabloo@gmail.com>
2018-09-27 17:48:39 -0300

    att 2.0.11

--- wpaq/MeAche/_js/createTable.js
+++ wpaq/MeAche/_js/createTable.js
@@ -7,13 +7,28 @@ firebase.database().ref('pedidos').on('value', function (snapshot) {
   var input_checkbox = document.createElement('input');
   var td_pedidos = document.createElement('td');
   var td_num_pedidos = document.createElement('td');
+
+
+  var ul = document.createElement('ul');
+  var li1 = document.createElement('li');
+  var li2 = document.createElement('li');
+  var li3 = document.createElement('li');
+  var li4 = document.createElement('li');
+  var li5 = document.createElement('li');
+  var li6 = document.createElement('li');
+
   var p = document.createElement('p');
 
+  ul.setAttribute("id", "ulAdmin");
+
   var linebreak = document.createElement('br');
-  var space = document.createTextNode('');
   
-  var petiscos_1 = document.createTextNode(item.val().Asinha_de_frango + "x" + " Asinha de Frango" ); 
-  var petiscos_2 = document.createTextNode(item.val().Camarao_Alho_Oleo + "x" + "Camarão Alho e Óleo" );
+  var petiscos_1 = document.createTextNode(item.val().Asinha_de_frango + "x" + " Asinha de Frango"); 
+  var petiscos_2 = document.createTextNode(item.val().Camarao_Alho_Oleo + "x" + " Camarão Alho e Óleo");
+  var petiscos_3 = document.createTextNode(item.val().Calabresa + "x" + " Calabresa");
+  var petiscos_4 = document.createTextNode(item.val().Carne_de_Sol + "x" + " Carne de Sol");
+  var petiscos_5 = document.createTextNode(item.val().Torresmo + "x" + " Torresmo");
+  var petiscos_6 = document.createTextNode(item.val().Batata_Frita + "x" + " Batata Frita");
 
   var trbody = document.getElementById('tableAdm');
 
@@ -23,6 +38,15 @@ firebase.database().ref('pedidos').on('value', function (snapshot) {
   tr.appendChild(td_num_pedidos);
   th.appendChild(input_checkbox);
   td_pedidos.appendChild(p);
+
+  td_pedidos.appendChild(ul);
+  ul.appendChild(li1); 
+  ul.appendChild(li2);
+  ul.appendChild(li3);
+  ul.appendChild(li4);
+  ul.appendChild(li5);
+  ul.appendChild(li6);
+
   th.setAttribute("scope", "row");
 
   input_checkbox.setAttribute("type", "checkbox");
@@ -43,9 +67,14 @@ firebase.database().ref('pedidos').on('value', function (snapshot) {
   }
 
   td_pedidos.setAttribute("colspan", "2");
-  p.appendChild(petiscos_1);
-  p.appendChild(linebreak);
-  p.appendChild(petiscos_2);
+
+  li1.appendChild(petiscos_1);  
+  li2.appendChild(petiscos_2);
+  li3.appendChild(petiscos_3);
+  li4.appendChild(petiscos_4);
+  li5.appendChild(petiscos_5);
+  li6.appendChild(petiscos_6); 
+
 
   input_checkbox.setAttribute("name", "number");
 
