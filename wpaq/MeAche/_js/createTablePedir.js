0f764cf87c0fdf2dd41922d500f9ab5e0244f09b
Johnny <Johnny.tec13@gmail.com>
2018-10-01 14:25:04 -0300

    Upando

--- wpaq/MeAche/_js/createTablePedir.js
+++ wpaq/MeAche/_js/createTablePedir.js
@@ -1,29 +1,33 @@
-function mandardados1(){
+function mandardados1(value){
 
 var value1 = document.getElementById('quantPet1').value;
+var preco1 = document.getElementById('preco1').value;
 var trbody = document.getElementById('tablePedidos');
 
+var tr = document.createElement('tr')
 var th = document.createElement('th')
 th.setAttribute("scope", "row");
-th.setAttribute("id", "qtd1");
-
-var tr = document.createElement('tr');
-var th = document.createElement('th');
+th.setAttribute("id", "th_qtd1");
 var td_Qtd1 = document.createElement('td');
 td_Qtd1.setAttribute("id", "petisco_1");
 var td_nameQtd1 = document.createElement('td');
 td_nameQtd1.setAttribute("id", "petisco_1.1");
+var td_preco1 = document.createElement('td');
+td_preco1.setAttribute("id","petisco_1.1.1");
 
 trbody.appendChild(tr);
 tr.appendChild(td_Qtd1);
 tr.appendChild(td_nameQtd1);
+tr.appendChild(td_preco1);
 
-var qtd = document.getElementById('petisco_1');
-qtd.innerText = value1;
-
+var qtd1 = document.getElementById('petisco_1');
+qtd1.innerText = value1;
 var asinha = document.getElementById('asinha').firstChild.innerHTML;
 document.getElementById('petisco_1.1').innerHTML = asinha;
+var precoValue1 = document.getElementById('petisco_1.1.1');
+precoValue1.innerText = preco1;
 
 td_Qtd1.style.textAlign="center";
 td_nameQtd1.style.textAlign="center";
+td_preco1.style.textAlign="center";
 }
