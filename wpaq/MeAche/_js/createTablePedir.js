1ee94f2a40a55320f944f3ffafb5e8cf6388fdbc
wpaq <wallysonpabloo@gmail.com>
2018-10-02 14:58:58 -0300

    2.2.4

--- wpaq/MeAche/_js/createTablePedir.js
+++ wpaq/MeAche/_js/createTablePedir.js
@@ -25,7 +25,9 @@ qtd1.innerText = value1;
 var asinha = document.getElementById('asinha').firstChild.innerHTML;
 document.getElementById('petisco_1.1').innerHTML = asinha;
 
-var  preco1 = document.getElementById('preco1').innerHTML;
+var preco1 = document.getElementById('preco1').innerHTML;
+
+
 document.getElementById('petisco_1.1.1').innerHTML = preco1;
 
 td_Qtd1.style.textAlign="center";
