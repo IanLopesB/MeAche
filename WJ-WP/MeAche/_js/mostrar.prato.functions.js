6f759356e1e187febe3c7d1d2ca473d37bc92037
Johnny <Johnny.tec13@gmail.com>
2018-10-05 16:46:39 -0300

    upando

--- WJ-WP/MeAche/_js/mostrar.prato.functions.js
+++ WJ-WP/MeAche/_js/mostrar.prato.functions.js
@@ -57,3 +57,226 @@ function fecharPrato6() {
     var e = document.getElementById("prato6");
     "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
 }
+
+function mostrarPrato7() {
+    var e = document.getElementById("prato7");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato7() {
+    var e = document.getElementById("prato7");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato8() {
+    var e = document.getElementById("prato8");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato8() {
+    var e = document.getElementById("prato8");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato9() {
+    var e = document.getElementById("prato9");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato9() {
+    var e = document.getElementById("prato6");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato10() {
+    var e = document.getElementById("prato10");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato10() {
+    var e = document.getElementById("prato10");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato11() {
+    var e = document.getElementById("prato11");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato11() {
+    var e = document.getElementById("prato11");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato12() {
+    var e = document.getElementById("prato12");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato12() {
+    var e = document.getElementById("prato12");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato13() {
+    var e = document.getElementById("prato13");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato13() {
+    var e = document.getElementById("prato13");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato14() {
+    var e = document.getElementById("prato14");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato14() {
+    var e = document.getElementById("prato14");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato15() {
+    var e = document.getElementById("prato15");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato15() {
+    var e = document.getElementById("prato15");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato16() {
+    var e = document.getElementById("prato16");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato16() {
+    var e = document.getElementById("prato16");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato17() {
+    var e = document.getElementById("prato17");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato17() {
+    var e = document.getElementById("prato17");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato18() {
+    var e = document.getElementById("prato18");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato18() {
+    var e = document.getElementById("prato18");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato19() {
+    var e = document.getElementById("prato19");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato19() {
+    var e = document.getElementById("prato19");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato20() {
+    var e = document.getElementById("prato20");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato20() {
+    var e = document.getElementById("prato20");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato21() {
+    var e = document.getElementById("prato21");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato21() {
+    var e = document.getElementById("prato21");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato22() {
+    var e = document.getElementById("prato22");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato22() {
+    var e = document.getElementById("prato22");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato23() {
+    var e = document.getElementById("prato23");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato23() {
+    var e = document.getElementById("prato23");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato24() {
+    var e = document.getElementById("prato24");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato24() {
+    var e = document.getElementById("prato24");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato25() {
+    var e = document.getElementById("prato25");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato25() {
+    var e = document.getElementById("prato25");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato26() {
+    var e = document.getElementById("prato26");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato26() {
+    var e = document.getElementById("prato26");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato27() {
+    var e = document.getElementById("prato27");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato27() {
+    var e = document.getElementById("prato27");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+function mostrarPrato28() {
+    var e = document.getElementById("prato28");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato28() {
+    var e = document.getElementById("prato28");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function mostrarPrato29() {
+    var e = document.getElementById("prato29");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
+
+function fecharPrato29() {
+    var e = document.getElementById("prato29");
+    "none" === e.style.display ? e.style.display = "block" : e.style.display = "none"
+}
