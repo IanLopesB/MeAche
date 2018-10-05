50aca8f49d447719c6f0933f3ac904f080ccaf98
wpaq <wallysonpabloo@gmail.com>
2018-10-05 17:01:15 -0300

    2.3.3

--- WJ-WP/MeAche/_js/tela.prato.functions.js
+++ WJ-WP/MeAche/_js/tela.prato.functions.js
@@ -57,3 +57,235 @@ function process6(quantPet6){
   document.getElementById("quantPet6").value = value;
   }
 }
+
+function process7(quantPet7){
+  var value = parseInt(document.getElementById("quantPet7").value);
+  value += quantPet7;
+  if(value < 1){
+    document.getElementById("quantPet7").value = 0;
+  }else{
+  document.getElementById("quantPet7").value = value;
+  }
+}
+
+function process8(quantPet8){
+  var value = parseInt(document.getElementById("quantPet8").value);
+  value += quantPet8;
+  if(value < 1){
+    document.getElementById("quantPet8").value = 0;
+  }else{
+  document.getElementById("quantPet8").value = value;
+  }
+}
+
+function process9(quantPet9){
+  var value = parseInt(document.getElementById("quantPet9").value);
+  value += quantPet9;
+  if(value < 1){
+    document.getElementById("quantPet9").value = 0;
+  }else{
+  document.getElementById("quantPet9").value = value;
+  }
+}
+
+function process10(quantPet10){
+  var value = parseInt(document.getElementById("quantPet10").value);
+  value += quantPet10;
+  if(value < 1){
+    document.getElementById("quantPet10").value = 0;
+  }else{
+  document.getElementById("quantPet10").value = value;
+  }
+}
+
+function process11(quantPet11){
+  var value = parseInt(document.getElementById("quantPet11").value);
+  value += quantPet11;
+  if(value < 11){
+    document.getElementById("quantPet11").value = 0;
+  }else{
+  document.getElementById("quantPet11").value = value;
+  }
+}
+
+function process12(quantPet12){
+  var value = parseInt(document.getElementById("quantPet12").value);
+  value += quantPet12;
+  if(value < 1){
+    document.getElementById("quantPet12").value = 0;
+  }else{
+  document.getElementById("quantPet12").value = value;
+  }
+}
+
+function process13(quantPet13){
+  var value = parseInt(document.getElementById("quantPet13").value);
+  value += quantPet13;
+  if(value < 1){
+    document.getElementById("quantPet13").value = 0;
+  }else{
+  document.getElementById("quantPet13").value = value;
+  }
+}
+
+function process14(quantPet14){
+  var value = parseInt(document.getElementById("quantPet14").value);
+  value += quantPet14;
+  if(value < 1){
+    document.getElementById("quantPet14").value = 0;
+  }else{
+  document.getElementById("quantPet14").value = value;
+  }
+}
+
+function process15(quantPet15){
+  var value = parseInt(document.getElementById("quantPet15").value);
+  value += quantPet15;
+  if(value < 1){
+    document.getElementById("quantPe1t5").value = 0;
+  }else{
+  document.getElementById("quantPet15").value = value;
+  }
+}
+
+function process16(quantPet16){
+  var value = parseInt(document.getElementById("quantPet16").value);
+  value += quantPet16;
+  if(value < 1){
+    document.getElementById("quantPet16").value = 0;
+  }else{
+  document.getElementById("quantPet16").value = value;
+  }
+}
+
+function process17(quantPet17){
+  var value = parseInt(document.getElementById("quantPet17").value);
+  value += quantPet17;
+  if(value < 1){
+    document.getElementById("quantPet17").value = 0;
+  }else{
+  document.getElementById("quantPet17").value = value;
+  }
+}
+
+function process18(quantPet18){
+  var value = parseInt(document.getElementById("quantPet18").value);
+  value += quantPet18;
+  if(value < 1){
+    document.getElementById("quantPet18").value = 0;
+  }else{
+  document.getElementById("quantPet18").value = value;
+  }
+}
+
+function process19(quantPet19){
+  var value = parseInt(document.getElementById("quantPet19").value);
+  value += quantPet19;
+  if(value < 1){
+    document.getElementById("quantPet19").value = 0;
+  }else{
+  document.getElementById("quantPet19").value = value;
+  }
+}
+
+function process20(quantPet20){
+  var value = parseInt(document.getElementById("quantPet20").value);
+  value += quantPet20;
+  if(value < 1){
+    document.getElementById("quantPet20").value = 0;
+  }else{
+  document.getElementById("quantPet20").value = value;
+  }
+}
+
+function process21(quantPet21){
+  var value = parseInt(document.getElementById("quantPet21").value);
+  value += quantPet21;
+  if(value < 1){
+    document.getElementById("quantPet21").value = 0;
+  }else{
+  document.getElementById("quantPet21").value = value;
+  }
+}
+
+function process2(quantPet22){
+  var value = parseInt(document.getElementById("quantPet22").value);
+  value += quantPet22;
+  if(value < 1){
+    document.getElementById("quantPet22").value = 0;
+  }else{
+  document.getElementById("quantPet22").value = value;
+  }
+}
+
+function process23(quantPet23){
+  var value = parseInt(document.getElementById("quantPet23").value);
+  value += quantPet23;
+  if(value < 1){
+    document.getElementById("quantPet23").value = 0;
+  }else{
+  document.getElementById("quantPet23").value = value;
+  }
+}
+
+function process24(quantPet24){
+  var value = parseInt(document.getElementById("quantPet24").value);
+  value += quantPet24;
+  if(value < 1){
+    document.getElementById("quantPet24").value = 0;
+  }else{
+  document.getElementById("quantPet24").value = value;
+  }
+}
+
+function process25(quantPet25){
+  var value = parseInt(document.getElementById("quantPet25").value);
+  value += quantPet25;
+  if(value < 1){
+    document.getElementById("quantPet25").value = 0;
+  }else{
+  document.getElementById("quantPet25").value = value;
+  }
+}
+
+function process26(quantPet26){
+  var value = parseInt(document.getElementById("quantPet26").value);
+  value += quantPet26;
+  if(value < 1){
+    document.getElementById("quantPet26").value = 0;
+  }else{
+  document.getElementById("quantPet26").value = value;
+  }
+}
+
+function process27(quantPet27){
+  var value = parseInt(document.getElementById("quantPet27").value);
+  value += quantPet27;
+  if(value < 1){
+    document.getElementById("quantPet27").value = 0;
+  }else{
+  document.getElementById("quantPet27").value = value;
+  }
+}
+
+function process28(quantPet28){
+  var value = parseInt(document.getElementById("quantPet28").value);
+  value += quantPet28;
+  if(value < 1){
+    document.getElementById("quantPet28").value = 0;
+  }else{
+  document.getElementById("quantPet28").value = value;
+  }
+}
+
+function process29(quantPet29){
+  var value = parseInt(document.getElementById("quantPet29").value);
+  value += quantPet29;
+  if(value < 1){
+    document.getElementById("quantPet29").value = 0;
+  }else{
+  document.getElementById("quantPet29").value = value;
+  }
+}
+
+
